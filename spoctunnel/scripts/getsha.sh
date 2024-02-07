53ee99af54315230a1a36cfe435fb32f28cd631d
Alan Janis <alan.janis@gmail.com>
2024-02-07 09:20:47 -0700

    add spoctunnel formula to repo

--- spoctunnel/scripts/getsha.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/ajanis/sshuttle-spoc-helper/archive/$1.tar.gz -O releases/$1.tar.gz
-shasum -a 256 releases/$1.tar.gz
--- spoctunnel/spoctunnel.rb
+++ spoctunnel/spoctunnel.rb
@@ -1,30 +1,36 @@
-
 require 'formula'
-class Spoctunnel < Formula
+class SshuttleSpocHelper < Formula
+  homepage "https://github.com/ajanis/spoctunnel"
+  url "https://github.com/ajanis/spoctunnel/archive/v1.1.1.tar.gz"
+  version "1.0.0"
+  sha256 "6453616e2592d139023b8c96464ce81383ee3a1faf3c79fb5bc6e694dfff2c52"
 
-  homepage "https://github.com/ajanis/homebrew-custombrew/sshuttle-spoc-helper"
-  url "https://github.com/ajanis/homebrew-custombrew/sshuttle-spoc-helper/archive/v1.1.1.tar.gz"
-  version "1.1.1"
+  # shasum -a 256 v1.1.tar.gz
+  version "1.0.0"
   sha256 "6453616e2592d139023b8c96464ce81383ee3a1faf3c79fb5bc6e694dfff2c52"
 
   # shasum -a 256 v1.1.tar.gz
 
   def install
-      install_dir = "#{etc}/sshuttle-spoc-helper"
-      FileUtils.rm_rf install_dir if install_dir.exist?
-      etc.install "sshuttle-spoc-helper"
-      bin.install 'spoctunnel_profile_setup.sh'
-      etc.install 'spoctunnel'
+    install_dir = "#{libexec}/spoctunnel"
+    FileUtils.rm_rf install_dir if install_dir.exist?
+    libexec.install Dir["spoctunnel"]
+
+    # Replace /libexec/ with HOMEBREW_PREFIX in scripts
+    inreplace "spoctunnel.sh", "/libexec/", HOMEBREW_PREFIX
+  inreplace "spoctunnel.sh", "spoctunnel_version", version
+    # Create an executable for the sshuttle-spoc-helper-profile-add.sh helper script
+    bin.write_exec_script (libexec/"spoctunnel/spoctunnel.sh"), bin/"spoctunnel"
+  end
+  test do
+    assert_match "Spoctunnel version 1.0.0", shell_output("#{bin}/spoctunnel version")
   end
 
-  def post_install
-      system "bash", "spoctunnel_profile_setup.sh"
-  end
 
-  def caveats; <<-EOS
-      Your shell profile had a couple of lines added to it to make `spoctunnel` available on the command line.
-      They will still be there after an uninstall, but are adaptive (nothing happens if spoctunnel is uninstalled)
-      EOS
-    end
+def caveats; <<-EOS
+You will need to set your SPOC Active-Directory user.  This can be done by answering script prompt,
+or by running or adding the following to your $SHELL profile export SPOCUSER="{SPOC Active-Directory Username}"
+EOS
+end
 
 end
