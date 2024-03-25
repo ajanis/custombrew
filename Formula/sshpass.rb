6b99f62febcfe7f95fcb4d76191ba75156ec9437
Alan Janis <alan.janis@gmail.com>
2024-03-25 12:15:06 -0600

    update too sourcefource repo

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
 class Sshpass < Formula
-  version "1.0.6"
-  homepage "https://github.com/ajanis/sshpass"
-  url "https://github.com/ajanis/sshpass/releases/download/v1.0.6/v1.0.6.tar.gz"
-  sha256 "011206b77e07abce0b61f697d91b8bbd1e516ae41a2c0f4e74d2e4ee4a033aca"
+  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.10/sshpass-1.10.tar.gz'
+  homepage 'http://sourceforge.net/projects/sshpass'
+  sha256 'ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
